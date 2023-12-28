1d6bea551149cddecfe03f596fb656927e8d5e7f
Dustin Wyatt <dustin.wyatt@gmail.com>
2023-12-28 13:37:56 -0600

    Use utf-8 encoding when reading README.md in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open('requirements.txt', encoding="utf-8") as f:
     requirements = f.read().splitlines()
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
